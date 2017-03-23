da83d4b6ad0328c1565ee643a36f178d5aac0723
mark medlin <mmedlin1997@yahoo.com>
2017-03-23 16:26:27 -0700

    Created a simple list view on the main list fragment.

--- app/src/main/java/com/tehah/notebook/MainActivityListFragment.java
+++ app/src/main/java/com/tehah/notebook/MainActivityListFragment.java
@@ -1,13 +1,11 @@
 package com.tehah.notebook;
 
-
 import android.app.ListFragment;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
-import com.tehah.notebook.R;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,8 +13,23 @@ import com.tehah.notebook.R;
 public class MainActivityListFragment extends ListFragment {
 
     @Override
+    // Define what happens the moment the activity is created
     public void onActivityCreated(Bundle savedInstanceState){
         super.onActivityCreated(savedInstanceState);
+
+        // Create an array of data for our list
+        String[] values = new String[]{"Red","Blue","Green","Orange","Violet",
+                "Yellow","Pink","Grey","Black","Red","Blue","Green","Orange","Violet",
+                "Yellow","Pink","Grey","Black"};
+
+        // Basic Array Adapter
+        // Use Androids ArrayAdapter to store the list. Constructor takes the activity that uses the
+        // data, the layout that shows the data just some <XML>, and the actual data.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(),
+                android.R.layout.simple_list_item_1, values);
+
+        // Tell the ListFragment to use the ArrayAdapter
+        setListAdapter(adapter);
     }
 
     @Override
